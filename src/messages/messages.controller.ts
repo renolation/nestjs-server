b17090c1addaab1d34d615020a53c1773d8e89ac
Phuoc Nguyen <vodanh.2901@gmail.com>
2022-06-16 10:25:39 +0700

    init cli

---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get, Post } from '@nestjs/common';
+
+@Controller('messages')
+export class MessagesController {
+
+    @Get()
+    listMessages(){
+
+    }
+
+    @Post()
+    createMessage(){
+
+    }
+
+    @Get('/:id')
+    getMessage(){
+
+    }
+
+}
