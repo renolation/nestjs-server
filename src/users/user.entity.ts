5871050b847625360d4a0b86fc78aa723b2b5d01
Phuoc Nguyen <vodanh.2901@gmail.com>
2022-06-23 00:45:04 +0700

    crud user

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,29 +1,36 @@
-import { AfterInsert, AfterRemove, AfterUpdate, Entity, Column, PrimaryGeneratedColumn} from 'typeorm';
-
-@Entity()
-export class User {
-
+import {
+    AfterInsert,
+    AfterRemove,
+    AfterUpdate,
+    Entity,
+    Column,
+    PrimaryGeneratedColumn,
+  } from 'typeorm';
+  
+  @Entity()
+  export class User {
     @PrimaryGeneratedColumn()
     id: number;
-
+  
     @Column()
     email: string;
-
+  
     @Column()
     password: string;
-
+  
     @AfterInsert()
-    logInsert(){
-        console.log('Inserted User with id', this.id);
+    logInsert() {
+      console.log('Inserted User with id', this.id);
     }
-
+  
     @AfterUpdate()
-    logUpdate(){
-        console.log('Updated User with id', this.id);
+    logUpdate() {
+      console.log('Updated User with id', this.id);
     }
-
+  
     @AfterRemove()
-    logRemove(){
-        console.log('Removed User with id', this.id);
+    logRemove() {
+      console.log('Removed User with id', this.id);
     }
-}
+  }
+  
